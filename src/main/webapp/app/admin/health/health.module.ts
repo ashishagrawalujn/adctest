ebe074fec288d352053b445e228bb209dda1e233
Ashish <ashish.agrawal@capgemini.com>
2020-10-14 14:57:02 +0530

    Initial version of adctest generated by JHipster-6.10.3

---
+++ src/main/webapp/app/admin/health/health.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AdctestSharedModule } from 'app/shared/shared.module';
+
+import { HealthComponent } from './health.component';
+import { HealthModalComponent } from './health-modal.component';
+
+import { healthRoute } from './health.route';
+
+@NgModule({
+  imports: [AdctestSharedModule, RouterModule.forChild([healthRoute])],
+  declarations: [HealthComponent, HealthModalComponent],
+  entryComponents: [HealthModalComponent],
+})
+export class HealthModule {}
